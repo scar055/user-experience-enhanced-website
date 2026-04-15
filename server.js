a9ce237892fa72bdc493e0c1e0b7c85dc97ea54e
Rowan <48519289+scar055@users.noreply.github.com>
2026-04-15 09:54:27 +0200

    add old routes

--- server.js
+++ server.js
@@ -26,6 +26,47 @@ app.get("/", async function (request, response) {
   response.render("index.liquid", {path: request.path});
 });
 
+app.get("/nieuws", async function (request, response) {
+  const params = {
+    "groupBy[]": "title,description,date",
+  };
+  const newsResponse = await fetch(
+    "https://fdnd-agency.directus.app/items/adconnect_news/?" +
+      new URLSearchParams(params),
+  );
+
+  response.render("news.liquid", {path: request.path});
+});
+
+app.get("/nieuws-toevoegen", async function (request, response) {
+  response.render("add-news.liquid", {path: request.path});
+});
+
+app.post("/nieuws-toevoegen", async function (request, response) {
+  await fetch("https://fdnd-agency.directus.app/items/adconnect_news", {
+    method: "POST",
+    body: JSON.stringify({
+      description: request.body.description,
+      body: request.body.body,
+      status: request.body.status,
+      titel: request.body.title,
+      author: request.body.author,
+      date: request.body.date,
+    }),
+
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  });
+  response.redirect(303, "/nieuws-toevoegen");
+});
+
+app.get("/lado", async function (request, response) {
+  // Render index.liquid uit de Views map
+
+  response.render("lado.liquid", {path: request.path});
+});
+
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
 app.set("port", process.env.PORT || 8000);
