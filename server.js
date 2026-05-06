18b0daadc16bb8de532cc1fc1ea9e8d17fac8c7e
Rowan <48519289+scar055@users.noreply.github.com>
2026-05-06 10:25:21 +0200

    remove comments and add news url #17

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
-// import express en liquid
 import express from "express";
 import {Liquid} from "liquidjs";
 
 // maak een express app aan
 const app = express();
 
-// Maak werken met data uit formulieren iets prettiger
 app.use(express.urlencoded({extended: true}));
-
-// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
-// Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static("public"));
 
 // Stel Liquid in als 'view engine'
 const engine = new Liquid();
 app.engine("liquid", engine.express());
 
-// Stel de map met Liquid templates in
 app.set("views", "./views");
 
-//voeg een index view aan
-app.get("/", async function (request, response) {
-  // Render index.liquid uit de Views map
+const newsURL = "https://fdnd-agency.directus.app/items/adconnect_news";
 
+app.get("/", async function (request, response) {
   response.render("index.liquid", {path: request.path});
 });
 
@@ -48,7 +41,7 @@ app.get("/nieuws-toevoegen", async function (request, response) {
 });
 
 app.post("/nieuws-toevoegen", async function (request, response) {
-  await fetch("https://fdnd-agency.directus.app/items/adconnect_news", {
+  await fetch(newsURL, {
     method: "POST",
     body: JSON.stringify({
       description: request.body.description,
@@ -71,17 +64,11 @@ app.get("/nieuws-toevoegen-succes", async function (request, response) {
 });
 
 app.get("/lado", async function (request, response) {
-  // Render index.liquid uit de Views map
-
   response.render("lado.liquid", {path: request.path});
 });
 
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
 app.set("port", process.env.PORT || 8000);
 
-// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get("port"), function () {
-  // Toon een bericht in de console
   console.log(`ga naar http://localhost:${app.get("port")}/`);
 });
